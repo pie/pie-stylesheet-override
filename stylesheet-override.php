0910fe0b15a43e2347ad16e97daad5468f682508
Neil Pie <neil.pie@gmail.com>
2012-07-31 09:11:24 +0200

    Update stylesheet-override.php
    Tweaked description

--- stylesheet-override.php
+++ stylesheet-override.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Pie Stylesheet Override
-Description: This plugin over-rides the default stylesheet in the theme, replacing it with that included here. This is to avoid having to create a child-theme.
+Description: This plugin over-rides the default stylesheet in the theme, replacing it with that included here.
 Version: 0.1
 Author: Neil Pie
 Author URI: http://neilpie.co.uk
